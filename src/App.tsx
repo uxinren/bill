9b230353a0f072d438e26ec7f66c9bff96e28425
long <315921205@qq.com>
2023-02-01 15:15:42 +0800

    解决浏览器开始不能全屏bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
+import "./App.scss"
 
 export const App = defineComponent({
      setup(){
-        return ()=>
-        <>
-         <header>导航
-            <ul>
-               <li>
-                  <router-link to="/Foo">Foo</router-link>
-               </li>
-               <li>
-                  <router-link to="/Bar">Bar</router-link>
-               </li>
-            </ul>   
-         </header> 
-         <div>
+        return ()=>(
+         <div class="page">
             <RouterView />
          </div>
-         <footer>页脚</footer>
-        </>
+        )
      }
 })
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,18 @@
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
-
+import s from "./Welcome.module.scss"
+import  logo from "../assets/icons/rainbow.svg"
 export const Welcome = defineComponent({
     setup(props, context) {
-       return ()=><>
-        <div><RouterView/></div>
-       </>
+       return ()=>
+        <div class={s.wrapper}>
+            <header>
+                <div class={s.logo}>
+                    <img src={logo} alt="logo" />
+                </div>
+                <h1>彩虹记账</h1>
+            </header>
+            <main class={s.main}><RouterView/></main>
+       </div>
     }
 })
