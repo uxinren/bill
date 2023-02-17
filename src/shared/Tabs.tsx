8edd96506563cc49726bdc562ce3abb82fd65fa6
long <315921205@qq.com>
2023-02-17 16:59:00 +0800

    完成tab组件中

---
+++ src/shared/Tabs.tsx
@@ -0,0 +1,49 @@
+import { defineComponent,PropType} from 'vue';
+import s from './Tabs.module.scss';
+export const Tabs = defineComponent({
+    props:{
+      selected:{
+        type: String as PropType<string>
+    },
+    onUpdateSelected:{
+        type: Function as PropType<(name:string)=>void>
+    }
+  },
+  setup:(props,context)=>{
+   
+    return ()=>{
+    const array = context.slots.default?.()
+    if(!array) return ()=>null
+    for (let i = 0; i < array.length; i++){
+        if(array[i].type !== Tab ){
+            throw new Error("Tabs只接受Tab子组件")
+        }
+    }
+    return <div class={s.tabs}>
+            <ol class={s.tabs_nav}>
+                {array.map(item =><li class={item.props?.name === props.selected?s.selected:''}
+                // onClick={()=>props.onUpdateSelected?.(item.props?.name)}
+                onClick={()=>context.emit('update:selected', item.props?.name)}
+                >
+                    {item.props?.name}
+                </li>)}
+            </ol>
+    </div>
+    }
+  }
+})
+
+
+import c from './Tab.module.scss';
+export const Tab = defineComponent({
+    props:{
+      name:{
+        type: String as PropType<string>
+    }
+  },
+  setup:(props,context)=>{
+    return ()=>(
+    <div>{context.slots.default?.()}</div>
+    )
+  }
+})
