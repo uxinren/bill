b896e91dca474b957ddcce6787ebda8a38e6a51b
long <315921205@qq.com>
2023-02-21 16:37:33 +0800

    完成日期选择组件

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,5 +1,7 @@
-import { defineComponent,PropType} from 'vue';
+import { defineComponent,PropType,ref} from 'vue';
+import { Popup ,DatetimePicker } from 'vant';
 import { Icon } from '../../shared/Icon';
+import { time } from '../../shared/Time'; 
 import s from './InputPad.module.scss';
 export const InputPad = defineComponent({
     props:{
@@ -8,31 +10,70 @@ export const InputPad = defineComponent({
     }
   },
   setup:(props,context)=>{
+    const now = new Date();
+    const refDate = ref<Date>(now);
+    const appendText = (n:number | string)=>{
+      const nString = n.toString()
+      const dotIndex = refAmount.value.indexOf('.')
+      if (refAmount.value.length >= 13) {
+        return
+      }
+      if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
+        return
+      }
+      if (nString === '.') {
+        if (dotIndex >= 0) { // 已经有小数点了
+          return
+        }
+      } else if (nString === '0') {
+        if (dotIndex === -1) { // 没有小数点
+          if (refAmount.value === '0') { // 没小数点，但是有0
+            return
+          }
+        }
+      } else {
+        if (refAmount.value === '0') {
+          refAmount.value = ''
+        }
+      }
+      refAmount.value += n.toString()
+    }
+    
     const buttons = [
-        {text: "1",onclick: () =>{}},
-        {text: "2",onclick: () =>{}},
-        {text: "3",onclick: () =>{}},
-        {text: "清空",onclick: () =>{}},
-        {text: "4",onclick: () =>{}},
-        {text: "5",onclick: () =>{}},
-        {text: "6",onclick: () =>{}},
-        {text: "+",onclick: () =>{}},
-        {text: "7",onclick: () =>{}},
-        {text: "8",onclick: () =>{}},
-        {text: "9",onclick: () =>{}},
-        {text: "-",onclick: () =>{}},
-        {text: ".",onclick: () =>{}},
-        {text: "0",onclick: () =>{}},
-        {text: "删除",onclick: () =>{}},
+        {text: "1",onclick: () =>{appendText(1)}},
+        {text: "2",onclick: () =>{appendText(2)}},
+        {text: "3",onclick: () =>{appendText(3)}},
+        {text: "4",onclick: () =>{appendText(4)}},
+        {text: "5",onclick: () =>{appendText(5)}},
+        {text: "6",onclick: () =>{appendText(6)}},
+        {text: "7",onclick: () =>{appendText(7)}},
+        {text: "8",onclick: () =>{appendText(8)}},
+        {text: "9",onclick: () =>{appendText(9)}},
+        {text: ".",onclick: () =>{appendText('.')}},
+        {text: "0",onclick: () =>{appendText(0)}},
+        {text: "清空",onclick: () =>{refAmount.value = ''}},
         {text: "提交",onclick: () =>{}}
     ]
+    const refDatePickerVisible = ref(false)
+    const columnsType = ['year', 'month','day']
+    const showDatePicker = () => refDatePickerVisible.value = true
+    const hideDatePicker = () => refDatePickerVisible.value = false
+    const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
+    const refAmount = ref('0')
     return ()=><>
     <div class={s.dateAndAmount}>
-        <span class={s.date}>
-            <Icon name='date' class={s.icon}/>
-            <span><input type="date"/></span>
+    <span class={s.date}>
+          <Icon name="date" class={s.icon} />
+          <span>
+            <span onClick={showDatePicker}>{time(refDate.value).format()}</span>
+            <Popup position='bottom' v-model:show={refDatePickerVisible.value}>
+              <DatetimePicker value={refDate.value} type='date' title='选择年月日'
+                onConfirm={setDate} onCancel={hideDatePicker}
+              />
+            </Popup>
+          </span>
         </span>
-        <div class={s.amount}>199.12</div>
+        <div class={s.amount}>{refAmount.value}</div>
     </div>
     <div class={s.buttons}>
         {buttons.map(button =>
--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -3,7 +3,7 @@ import s from './ItemList.module.scss';
 export const ItemList = defineComponent({
     props:{
       name:{
-        type: String as PropType<'add'|'chart'|'clock'|'cloud'>
+        type: String as PropType<string>
     }
   },
   setup:(props,context)=>{
