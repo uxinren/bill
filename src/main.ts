6702d1e72ae32487d8015835af9ed3d59d4d81f8
long <315921205@qq.com>
2023-03-10 16:12:53 +0800

    每个路由都做了登录检查

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,27 @@ import { App } from './App'
 import { createRouter } from 'vue-router'
 import { history } from './shared/history';
 import '@svgstore';
+import { defaultHttpClient } from './shared/HttpClient';
+import { fetchMe, mePromise } from './shared/me';
 
 const router = createRouter({ history, routes })
 
+fetchMe()
+
+router.beforeEach(async (to,from)=>{
+    if(to.path ==='/'
+    || to.path==='/start'
+    || to.path.startsWith('/welcome') 
+    || to.path.startsWith('/sign_in') ){
+        return true
+    }else{
+        const path = await mePromise!.then(
+            ()=>true,
+            ()=> '/sign_in?return_to=' + to.path
+        )
+        return path
+    }
+})
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
