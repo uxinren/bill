c2cc40f18f903c1d138a976f1db4833b1234a165
long <315921205@qq.com>
2023-03-18 17:03:51 +0800

    格式化文档

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -13,29 +13,30 @@ export const ItemCreate = defineComponent({
   setup: (props, context) => {
     //初始化类型、是否更多、当前页
     const formData = reactive({
-      kind: '支出',
+      kind: "支出",
       tag_id: [],
       happen_at: new Date().toISOString(),
-      amount: 0
-    })
-    const router = useRouter()
+      amount: 0,
+    });
+    const router = useRouter();
     const onError = (error: AxiosError<ResourceError>) => {
       if (error.response?.status === 422) {
         Dialog.alert({
-          title: '出错',
-          message: Object.values(error.response.data.errors).join('\n'),
+          title: "出错",
+          message: Object.values(error.response.data.errors).join("\n"),
         });
       }
       throw error;
     };
     //发送金额数据
     const onSubmit = async () => {
-      await defaultHttpClient.post<Resource<Item>>('/items', formData,
-      {params: { _mock: 'itemCreate'
-    }}
-      ).catch(onError)
-      router.push('/items')
-    }
+      await defaultHttpClient
+        .post<Resource<Item>>("/items", formData, {
+          params: { _mock: "itemCreate" },
+        })
+        .catch(onError);
+      router.push("/items");
+    };
     return () => (
       <MainLayout class={s.layout}>
         {{
@@ -50,17 +51,24 @@ export const ItemCreate = defineComponent({
                   selected={formData.kind}
                   class={s.tabs}
                 >
-                  <Tab name="支出" >
-                   <Tags kind='expenses' v-model:selected={formData.tag_id[0]}></Tags>
+                  <Tab name="支出">
+                    <Tags
+                      kind="expenses"
+                      v-model:selected={formData.tag_id[0]}
+                    ></Tags>
                   </Tab>
-                  <Tab name="收入" >
-                    <Tags kind='income' v-model:selected={formData.tag_id[0]}></Tags>
+                  <Tab name="收入">
+                    <Tags
+                      kind="income"
+                      v-model:selected={formData.tag_id[0]}
+                    ></Tags>
                   </Tab>
                 </Tabs>
                 <div class={s.inputPad_wrapper}>
-                  <InputPad v-model:happenAt={formData.happen_at}
-                            v-model:amount ={formData.amount}
-                            onSubmit={onSubmit}
+                  <InputPad
+                    v-model:happenAt={formData.happen_at}
+                    v-model:amount={formData.amount}
+                    onSubmit={onSubmit}
                   />
                 </div>
               </div>
--- src/shared/HttpClient.tsx
+++ src/shared/HttpClient.tsx
@@ -1,90 +1,126 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+} from "axios";
 import { mockSession, mockTagIndex } from "../mock/mock";
 import { mockItemCreate } from "../mock/mockItemCreate";
 
-type GetConfig = Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>
-type PostConfig = Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
-type PatchConfig = Omit<AxiosRequestConfig, 'url' | 'data'>
-type DeleteConfig = Omit<AxiosRequestConfig, 'params'>
+type GetConfig = Omit<AxiosRequestConfig, "params" | "url" | "method">;
+type PostConfig = Omit<AxiosRequestConfig, "url" | "data" | "method">;
+type PatchConfig = Omit<AxiosRequestConfig, "url" | "data">;
+type DeleteConfig = Omit<AxiosRequestConfig, "params">;
 
 export class HttpClient {
-    instance : AxiosInstance
-    constructor(baseURL:string){
-        this.instance = axios.create({
-            baseURL
-        })
-    }
-    //查询
-    get<R = unknown>(url:string,query?:Record<string,JSONValue>,config?: GetConfig){
-        return this.instance.request<R>({...config,url,params:query,method:'get'})
-    }
-    //创建
-    post<R = unknown>(url:string,data?:Record<string,JSONValue>,config?:PostConfig){
-        return this.instance.request<R>({...config,url,data,method:'post'})
-    }
-    //更新
-    patch<R = unknown>(url:string,data?:Record<string,JSONValue>,config?:PatchConfig){
-            return this.instance.request<R>({...config,url,data,method:'patch'})
-        }
-    //删除
-    delete<R = unknown>(url:string,data?:Record<string,string>,config?:DeleteConfig){
-            return this.instance.request<R>({...config,url,data,method:'delete'})
-        }
+  instance: AxiosInstance;
+  constructor(baseURL: string) {
+    this.instance = axios.create({
+      baseURL,
+    });
+  }
+  //查询
+  get<R = unknown>(
+    url: string,
+    query?: Record<string, JSONValue>,
+    config?: GetConfig
+  ) {
+    return this.instance.request<R>({
+      ...config,
+      url,
+      params: query,
+      method: "get",
+    });
+  }
+  //创建
+  post<R = unknown>(
+    url: string,
+    data?: Record<string, JSONValue>,
+    config?: PostConfig
+  ) {
+    return this.instance.request<R>({ ...config, url, data, method: "post" });
+  }
+  //更新
+  patch<R = unknown>(
+    url: string,
+    data?: Record<string, JSONValue>,
+    config?: PatchConfig
+  ) {
+    return this.instance.request<R>({ ...config, url, data, method: "patch" });
+  }
+  //删除
+  delete<R = unknown>(
+    url: string,
+    data?: Record<string, string>,
+    config?: DeleteConfig
+  ) {
+    return this.instance.request<R>({ ...config, url, data, method: "delete" });
+  }
 }
 //接口联调测试
 const mock = (response: AxiosResponse) => {
-    if (location.hostname !== 'localhost'
-      && location.hostname !== '127.0.0.1'
-      && location.hostname !== '172.18.0.2') { return false }
-    switch (response.config?.params?._mock) {
-      case 'tagIndex':
-        [response.status, response.data] = mockTagIndex(response.config)
-        return true
-      case 'session':
-        [response.status, response.data] = mockSession(response.config)
-        return true
-      case 'itemCreate':
-        [response.status, response.data] = mockItemCreate(response.config)
-        return true
-    }
-    return false
+  if (
+    location.hostname !== "localhost" &&
+    location.hostname !== "127.0.0.1" &&
+    location.hostname !== "172.18.0.2"
+  ) {
+    return false;
   }
+  switch (response.config?.params?._mock) {
+    case "tagIndex":
+      [response.status, response.data] = mockTagIndex(response.config);
+      return true;
+    case "session":
+      [response.status, response.data] = mockSession(response.config);
+      return true;
+    case "itemCreate":
+      [response.status, response.data] = mockItemCreate(response.config);
+      return true;
+  }
+  return false;
+};
 
-export const defaultHttpClient = new HttpClient('/api/v1')
+export const defaultHttpClient = new HttpClient("/api/v1");
 
 //请求数据
-defaultHttpClient.instance.interceptors.request.use(config => {
-    const jwt = localStorage.getItem('jwt')
-    if (jwt) {
-      config.headers!.Authorization = `Bearer ${jwt}`
-    }
-    return config
-  })
-  
-defaultHttpClient.instance.interceptors.response.use((response) => {
-    mock(response)
-    if(response.status>=400){
-      throw{response}
-    }else{
-      return response
-    }
-  }, (error) => {
-    mock(error.response)
-    if (error.response.status >= 400) {
-      throw error
+defaultHttpClient.instance.interceptors.request.use((config) => {
+  const jwt = localStorage.getItem("jwt");
+  if (jwt) {
+    config.headers!.Authorization = `Bearer ${jwt}`;
+  }
+  return config;
+});
+
+defaultHttpClient.instance.interceptors.response.use(
+  (response) => {
+    mock(response);
+    if (response.status >= 400) {
+      throw { response };
     } else {
-      return error.response
+      return response;
     }
-  })
+  },
+  (error) => {
+    mock(error.response);
+    if (error.response.status >= 400) {
+      throw error;
+    } else {
+      return error.response;
+    }
+  }
+);
 //响应数据
-defaultHttpClient.instance.interceptors.response.use(response=>{
-    return response
-},(error)=>{
-    if(error.response){
-        const axiosError = error as AxiosError
-        if(axiosError.response?.status === 429){
-            console.log('请求过于频繁');
-        }
+defaultHttpClient.instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response?.status === 429) {
+        console.log("请求过于频繁");
+      }
     }
-        throw error
-    })
+    throw error;
+  }
+);
