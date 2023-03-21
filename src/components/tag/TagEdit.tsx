b6af89fd303479ca00fbbe310299a2efbc77d019
long <315921205@qq.com>
2023-03-21 14:35:58 +0800

    删除功能完成

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,31 +1,63 @@
-import { defineComponent} from 'vue';
-import { useRoute } from 'vue-router';
-import { MainLayout } from '../../layouts/MainLayout';
-import { BackIcon } from '../../shared/BackIcon';
-import { Button } from '../../shared/Button';
-import s from './Tag.module.scss';
-import { TagForm } from './TagForm';
+import { Dialog } from "vant";
+import { defineComponent } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { MainLayout } from "../../layouts/MainLayout";
+import { BackIcon } from "../../shared/BackIcon";
+import { Button } from "../../shared/Button";
+import { defaultHttpClient } from "../../shared/HttpClient";
+import s from "./Tag.module.scss";
+import { TagForm } from "./TagForm";
 export const TagEdit = defineComponent({
-  setup:(props,context)=>{
-    const route = useRoute()
-    const numberId = parseInt(route.params.id!.toString())
-    if(Number.isNaN(numberId)){
-      return ()=>{
-        <div>id不存在</div>
-      }
+  setup: (props, context) => {
+    const route = useRoute();
+    const numberId = parseInt(route.params.id!.toString());
+    if (Number.isNaN(numberId)) {
+      return () => {
+        <div>id不存在</div>;
+      };
     }
-    return ()=>(
-    <MainLayout>{{
-        title:()=>'编辑标签',
-        icon: ()=><BackIcon />,
-        default:()=><>
-            <TagForm id={numberId}/>
-            <div class={s.action}>
-              <Button level='danger' onClick={()=>{}}>标签删除</Button>
-              <Button level='danger' onClick={()=>{}}>全部删除</Button>
-            </div>
-        </>
-    }}</MainLayout>
-    )
-  }
-})
+    const onError = () => {
+      Dialog.alert({
+        title: "错误",
+        message: "id不存在",
+      });
+    };
+    const router = useRouter();
+    const onDelete = async (option?: { withItem: boolean }) => {
+      await Dialog.confirm({
+        title: "删除标签",
+        message: "确认删除标签吗？",
+      });
+      await defaultHttpClient
+        .delete(`/tags/${numberId}`, {
+          withItem: option?.withItem ? "true" : "false",
+        })
+        .catch(onError);
+      router.back();
+    };
+    return () => (
+      <MainLayout>
+        {{
+          title: () => "编辑标签",
+          icon: () => <BackIcon />,
+          default: () => (
+            <>
+              <TagForm id={numberId} />
+              <div class={s.action}>
+                <Button level="danger" onClick={() => onDelete()}>
+                  删除标签
+                </Button>
+                <Button
+                  level="danger"
+                  onClick={() => onDelete({ withItem: true })}
+                >
+                  全部删除
+                </Button>
+              </div>
+            </>
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});
